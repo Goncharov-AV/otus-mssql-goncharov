6f5331e13b0b9cbfae4622b5886ceb6a38b33006
Alexander Goncharov <Alexander.Goncharov@evraz.com>
2022-01-23 16:20:44 +0300

    HW11 Question

--- HW11 - procedures/hw_stored_procedures_tasks-188-abcad7.sql
+++ HW11 - procedures/hw_stored_procedures_tasks-188-abcad7.sql
@@ -14,7 +14,7 @@ https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importer
 * https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
 */
 
-USE WideWorldImporters
+USE WideWorldImporters;
 
 /*
 Во всех заданиях написать хранимую процедуру / функцию и продемонстрировать ее использование.
@@ -23,9 +23,31 @@ USE WideWorldImporters
 /*
 1) Написать функцию возвращающую Клиента с наибольшей суммой покупки.
 */
+GO;
 
-напишите здесь свое решение
+IF OBJECT_ID ( 'Sales.CustomerWithMaxSum', 'FN' ) IS NOT NULL   
+    DROP FUNCTION Sales.CustomerWithMaxSum; 
+GO;
 
+CREATE FUNCTION Sales.CustomerWithMaxSum()
+RETURNS int
+AS
+BEGIN
+	RETURN 
+	(
+		SELECT TOP(1)
+			C.CustomerID
+		FROM Sales.Customers AS C
+		JOIN Sales.Invoices AS I ON I.CustomerID = C.CustomerID
+		JOIN Sales.InvoiceLines AS L ON L.InvoiceID = I.InvoiceID
+		GROUP BY C.CustomerID
+		ORDER BY SUM(L.Quantity * L.UnitPrice) DESC
+	)
+END;
+GO;
+
+SELECT Sales.CustomerWithMaxSum() AS [Customer with max invoice sum];
+GO;
 /*
 2) Написать хранимую процедуру с входящим параметром СustomerID, выводящую сумму покупки по этому клиенту.
 Использовать таблицы :
@@ -34,19 +56,96 @@ Sales.Invoices
 Sales.InvoiceLines
 */
 
-напишите здесь свое решение
+IF OBJECT_ID ( 'Sales.GetSumByCustomerId', 'FN' ) IS NOT NULL   
+    DROP FUNCTION Sales.GetSumByCustomerId; 
+GO;
+
+CREATE FUNCTION Sales.GetSumByCustomerId
+(
+	@custId INT
+)
+RETURNS DECIMAL(18,2)
+AS
+BEGIN
+	RETURN 
+	(
+		SELECT TOP(1)
+			SUM(L.Quantity * L.UnitPrice)
+		FROM Sales.Customers AS C
+		JOIN Sales.Invoices AS I ON I.CustomerID = C.CustomerID
+		JOIN Sales.InvoiceLines AS L ON L.InvoiceID = I.InvoiceID
+		WHERE C.CustomerID = @custId
+		GROUP BY C.CustomerID
+	)
+END;
+GO;
+
+SELECT C.CustomerID, C.CustomerName, Sales.GetSumByCustomerId(C.CustomerID) AS [Sum by Customer ID] FROM Sales.Customers AS C;
+GO;
 
 /*
 3) Создать одинаковую функцию и хранимую процедуру, посмотреть в чем разница в производительности и почему.
 */
+IF OBJECT_ID ( 'Sales.GetSumsByPattern', 'IF' ) IS NOT NULL   
+    DROP FUNCTION Sales.GetSumsByPattern; 
+GO
 
-напишите здесь свое решение
+CREATE FUNCTION Sales.GetSumsByPattern
+(
+	@pattern varchar(50)
+)
+RETURNS TABLE
+AS
+RETURN 
+(
+	SELECT
+		MAX(C.CustomerName) AS CustomerName,
+		SUM(L.Quantity * L.UnitPrice) AS [Sum]
+	FROM Sales.Customers AS C
+	JOIN Sales.Invoices AS I ON I.CustomerID = C.CustomerID
+	JOIN Sales.InvoiceLines AS L ON L.InvoiceID = I.InvoiceID
+	WHERE C.CustomerName LIKE CONCAT(@pattern, '%')
+	GROUP BY C.CustomerID
+);
+GO
+
+IF OBJECT_ID ( 'Sales.GetSumsByPatternP', 'P' ) IS NOT NULL   
+    DROP PROCEDURE Sales.GetSumsByPatternP; 
+GO
+
+CREATE PROCEDURE Sales.GetSumsByPatternP
+(
+	@pattern varchar(50)
+)
+AS
+BEGIN
+		SELECT
+			MAX(C.CustomerName) AS CustomerName,
+			SUM(L.Quantity * L.UnitPrice) AS [Sum]
+		FROM Sales.Customers AS C
+		JOIN Sales.Invoices AS I ON I.CustomerID = C.CustomerID
+		JOIN Sales.InvoiceLines AS L ON L.InvoiceID = I.InvoiceID
+		WHERE C.CustomerName LIKE CONCAT(@pattern, '%')
+		GROUP BY C.CustomerID;
+END;
+GO
+
+SET STATISTICS time, io ON;
+SELECT * FROM Sales.GetSumsByPattern('La');
+EXECUTE Sales.GetSumsByPatternP @pattern = 'La';
+SET STATISTICS time, io OFF;
 
 /*
 4) Создайте табличную функцию покажите как ее можно вызвать для каждой строки result set'а без использования цикла. 
 */
 
-напишите здесь свое решение
+EXEC ('SELECT CustomerID,CustomerName,CustomerCategoryID FROM Sales.Customers WHERE CustomerCategoryID = 3')
+WITH RESULT SETS
+((
+	[identity] int,
+	[name] nvarchar(50),
+	[category] int
+))
 
 /*
 5) Опционально. Во всех процедурах укажите какой уровень изоляции транзакций вы бы использовали и почему. 
